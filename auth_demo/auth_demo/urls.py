571a5e51ad20446ff17b01af59712fc2e121524d
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2025-07-03 14:59:39 +0800

    first commit

---
+++ auth_demo/auth_demo/urls.py
@@ -0,0 +1,8 @@
+# auth_demo/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+]
